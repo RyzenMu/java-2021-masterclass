a7ef7c351a0bc90f6d528a80f0d2a24f61ed4602
RyzenMu <muruga13in2022@gmail.com>
2023-12-24 10:03:27 +0530

    variable in java

---
+++ VarablesAndDataTypes/src/Variable.java
@@ -0,0 +1,15 @@
+public class Variable {
+    public static void main(String[] args) {
+        // variables are also data types
+        // 1. Numeric data
+        // 2. Text data
+        // 3. Collection Type
+        // data type int, short, double
+        // data_type variable_name = value;
+        int my_age = 26;
+        int my_number = 17356; // int is a numeric data
+        short a = 4;
+        short cat = 99; // short is also numeric data
+
+    }
+}
