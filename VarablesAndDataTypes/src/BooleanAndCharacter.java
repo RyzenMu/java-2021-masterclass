a5518b78f957eed247c3e3561bb88b24b19c4e95
RyzenMu <muruga13in2022@gmail.com>
2023-12-24 11:05:08 +0530

    variable in java

---
+++ VarablesAndDataTypes/src/BooleanAndCharacter.java
@@ -0,0 +1,18 @@
+public class BooleanAndCharacter {
+    public static void main(String[] args) {
+        /*
+        * char - text - 2 byte
+        * boolean - bool - true or false - 1 byte
+        * */
+
+        char c = 4;
+        System.out.println(c);
+        char c1 = 65;
+        System.out.println(c1); // prints 'A'
+        char c2 = 77;
+        System.out.println(c2);
+
+        boolean b = true;
+        System.out.println(b);
+    }
+}
