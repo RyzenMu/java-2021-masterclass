9d80d3bbd59419543afc45afba5fefac9d66a18e
RyzenMu <muruga13in2022@gmail.com>
2023-12-24 10:48:23 +0530

    variable in java

---
+++ VarablesAndDataTypes/src/NumericDataTypes2.java
@@ -0,0 +1,33 @@
+public class NumericDataTypes2 {
+    public static void main(String[] args) {
+        /*
+         * byte  numeric  1 byte
+         * short - numeric - 2 byte
+         * int - numeric - 4 byte
+         * long  numeric  8 bytes
+         * float  - float number  - 4 bytes
+         * double - decimal number - 8 bytes
+         * */
+        int my_age = 26;
+        int my_number = 17356; // int is a numeric data
+        short a = 4;
+        short cat = 99; // short is also numeric data
+        System.out.println(cat);
+        System.out.println(my_age);
+
+        byte b = 77;
+        System.out.println(b);
+
+        long almond = 756378263;
+        System.out.println(almond);
+
+        float f1 = 5;
+        float f2 = 51;
+        System.out.println(f1 + f2);
+
+        double d = 5.55;
+        System.out.println(d);
+
+
+    }
+}
