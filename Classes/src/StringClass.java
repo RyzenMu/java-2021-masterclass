db7d7b3a72df5a8d3d31bfad96a5d265804643a4
RyzenMu <muruga13in2022@gmail.com>
2023-12-24 14:49:38 +0530

    StringClass class

---
+++ Classes/src/StringClass.java
@@ -0,0 +1,9 @@
+public class StringClass {
+    public static void main(String[] args) {
+        String s = "This is a String";
+        System.out.println(s);
+        String s1 = "A";
+        System.out.println(s1);
+
+    }
+}
