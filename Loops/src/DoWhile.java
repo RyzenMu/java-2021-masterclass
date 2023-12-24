0b051b6ab839d3b8a9c4b70d4f7db6e9ed4aa2f1
RyzenMu <muruga13in2022@gmail.com>
2023-12-24 17:43:35 +0530

    do while loop

--- Loops/src/DoWhile.java
+++ Loops/src/DoWhile.java
@@ -1,2 +1,9 @@
 public class DoWhile {
+    public static void main(String[] args) {
+        int i = 5;
+        do {
+            System.out.println("This is a do while loop");
+            i++;
+        }while (i<=2);
+    }
 }
