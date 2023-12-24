840bd6067cd3482d910f51795d01cf43e398a194
RyzenMu <muruga13in2022@gmail.com>
2023-12-24 12:10:09 +0530

    IncrementDecrementOperators

---
+++ VarablesAndDataTypes/src/IncrementDecrementOperators.java
@@ -0,0 +1,13 @@
+public class IncrementDecrementOperators {
+    public static void main(String[] args) {
+        int i = 9;
+        System.out.println(i++);
+        System.out.println(++i);
+        i+=5;
+        System.out.println(i);
+        i--;
+        System.out.println(i);
+        i/=2;
+        System.out.println(i);
+    }
+}
