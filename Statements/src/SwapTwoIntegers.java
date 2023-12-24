e7765c05f5db233cb9996e9d6d28987c42ba088f
RyzenMu <muruga13in2022@gmail.com>
2023-12-24 16:49:20 +0530

    SwapTwoIntegers

---
+++ Statements/src/SwapTwoIntegers.java
@@ -0,0 +1,12 @@
+public class SwapTwoIntegers {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 100;
+        int temp;
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println("The value of a is " + a);
+        System.out.println("The value of b is " + b);
+    }
+}
