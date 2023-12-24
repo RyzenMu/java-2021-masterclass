49aa80214f9daa04fde3885c5fe4928394dace0a
RyzenMu <muruga13in2022@gmail.com>
2023-12-24 11:55:52 +0530

    ArithmeticOperators

---
+++ VarablesAndDataTypes/src/ArithmeticOperators.java
@@ -0,0 +1,23 @@
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        double d = 5.6;
+        double b = 5;
+        System.out.println(d+b);
+        int a = 77;
+        int b1 = 99;
+        int c = a - b1;
+        System.out.println(c);
+        byte by1 = 43;
+        byte by2 = 54;
+        int by3 = by1 + by2;
+        System.out.println(by3);
+        double d2 = 452.6315;
+        double d1 = 15.54;
+        System.out.println(d1/d2);
+        char c1 ='A';
+        char c2 = 'B';
+        System.out.println(c1-c2);
+        System.out.println(c1+c2);
+
+    }
+}
