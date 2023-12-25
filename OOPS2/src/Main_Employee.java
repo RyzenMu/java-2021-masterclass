e0a297cc8bdb6e6ddad402d2cc2579488c6cadd0
RyzenMu <muruga13in2022@gmail.com>
2023-12-25 18:04:51 +0530

    employee application

--- OOPS2/src/Main_Employee.java
+++ OOPS2/src/Main_Employee.java
@@ -1,11 +1,14 @@
 public class Main_Employee {
     public static void main(String[] args) {
-        Employee e1 = new Employee("Quota", "Girda", 43);
-        e1.work();
-        Programmer p1 = new Programmer("Cats", "pasf", 46, "C++");
-        p1.work();
-        e1.showInfo();
-        p1.showInfo();
+        Employee employee = new Employee("Quota", "Girda", 43);
+        employee.work();
+        Programmer programmer = new Programmer("Cats", "pasf", 46, "C++");
+        programmer.work();
+        employee.showInfo();
+        programmer.showInfo();
+        CEO ceo = new CEO("Bob", "Marley", 40, 1700);
+        ceo.showInfo();
+        ceo.work();
 
 
     }
