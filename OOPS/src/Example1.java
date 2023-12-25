ebe386998fd7b20f61ce6d5beb870bf8a7ef0c7c
RyzenMu <muruga13in2022@gmail.com>
2023-12-25 12:04:14 +0530

    Classes and objects

---
+++ OOPS/src/Example1.java
@@ -0,0 +1,17 @@
+public class Example1 {
+    public static void main(String[] args) {
+        Example1 A1 = new Example1();
+
+        Student s1 = new Student();
+        s1.age = 44;
+        s1.email = "ssacademy_s1@gmail.com";
+        s1.name = "s1";
+
+        Student s2 = new Student();
+        s2.age = 24;
+        s2.email = "ssacademy_s2@gmail.com";
+        s2.name = "s2";
+        //private int rollNo is not available outside Student class
+
+    }
+}
---
+++ OOPS/src/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    public String name; // public is an identifier
+    public String surname; // public is an indentifier
+    public String email; // public is an identifier
+    public int age; // public is an identifier
+
+    private int rollNo;
+
+
+
+}
