ce89c2b80c91be02b64036904b3199affbff8250
RyzenMu <muruga13in2022@gmail.com>
2023-12-25 13:57:24 +0530

    this keyword

--- OOPS/src/PhoneConstructor.java
+++ OOPS/src/PhoneConstructor.java
@@ -1,5 +1,9 @@
 public class PhoneConstructor {
+
+
     public int size;
+    public String name;
+    public String color;
 
     public PhoneConstructor(int size, String name, String color) {
         this.size = size;
@@ -7,7 +11,8 @@ public class PhoneConstructor {
         this.color = color;
     }
 
-    public String name;
-    public String color;
+   public void print(){
+       System.out.println("I have a "+ this.name +"Phone of  " +this.color +"which have a size of "+this.size);
+   }
 
 }
--- OOPS/src/UsingThis.java
+++ OOPS/src/UsingThis.java
@@ -1,2 +1,8 @@
 public class UsingThis {
+    public static void main(String[] args) {
+        PhoneConstructor sam = new PhoneConstructor(6,"A03S", "Black");
+        PhoneConstructor nok = new PhoneConstructor(3, "N95", "White");
+        sam.print();
+        nok.print();
+    }
 }
