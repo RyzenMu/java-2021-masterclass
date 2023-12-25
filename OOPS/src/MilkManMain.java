4bec8b394c906576b962578193aa8ec1f872ebd7
RyzenMu <muruga13in2022@gmail.com>
2023-12-25 14:19:26 +0530

    getters and setters

---
+++ OOPS/src/MilkManMain.java
@@ -0,0 +1,9 @@
+public class MilkManMain {
+    public static void main(String[] args) {
+        MilkMan m1 = new MilkMan();
+        m1.setName("Samy");
+        m1.setAge(34);
+        System.out.println(m1.getName());
+        System.out.println(m1.getAge());
+    }
+}
