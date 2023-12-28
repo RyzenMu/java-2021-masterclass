36ba0844b89c749b20c75484165ac9767f3a3e0a
RyzenMu <muruga13in2022@gmail.com>
2023-12-28 06:15:30 +0530

    wrapper and behaviour

---
+++ OOPS2/src/StatesAndBehaviour.java
@@ -0,0 +1,18 @@
+public class StatesAndBehaviour {
+    //states are also called  as variable
+    private String name;
+    private  String surname;
+    //behavior also called as methods or function 
+    public void walk(){
+        System.out.println("person is walking" +
+                "gf");
+    }
+
+    public StatesAndBehaviour(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public StatesAndBehaviour() {
+    }
+}
--- OOPS2/src/Wrapper.java
+++ OOPS2/src/Wrapper.java
@@ -1,3 +1,15 @@
 public class Wrapper {
+//prinmitive daa pes
+    //in
+    //boolean
+    //floa
+    //double ec
+    //wrapper classes
+    //INEGER
+    //FLOA
+    //DOUBLE ec
+    int h=4;
+    //int g = null; null is of different data type
+    Integer in; //wrapper class can have mehods like compare, ...
 
-}
+
