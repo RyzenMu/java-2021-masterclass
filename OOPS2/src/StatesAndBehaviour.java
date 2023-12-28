5711d782528bf22e5030fa41e0d37b7a1865894c
RyzenMu <muruga13in2022@gmail.com>
2023-12-29 04:59:22 +0530

    wrapper and behaviour

--- OOPS2/src/StatesAndBehaviour.java
+++ OOPS2/src/StatesAndBehaviour.java
@@ -2,6 +2,7 @@ public class StatesAndBehaviour {
     //states are also called  as variable
     private String name;
     private  String surname;
+    private int age;
     //behavior also called as methods or function 
     public void walk(){
         System.out.println("person is walking" +
