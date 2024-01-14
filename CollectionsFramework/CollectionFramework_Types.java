cc43718de2b0380dbf3ae92ddac6759fdc625f40
RyzenMu <106828992+RyzenMu@users.noreply.github.com>
2024-01-14 14:09:32 +0530

    Java Framework types

---
+++ CollectionsFramework/CollectionFramework_Types.java
@@ -0,0 +1,15 @@
+public class CollectionFramework_Types {
+
+    /*
+    *                                  collection
+    *                                       |
+    * ---------------------------------------------------------------
+    * |                            |                                |
+    * List                      Queue                              Set                            Map
+    * |-Array List               |-Linked List                    |-SortedSet                    |-sorted Map
+    * |-Vector-stack             |-priority Queue                 |-TreeSet                      |-TreeSet
+    * |-Linked List              |-Deque                          |-HashSet                      |-HashMap
+    *                                                             |-LinkedHashset                |-hashTable
+    *                                                                                            |-Linked hash map
+    * */
+}
