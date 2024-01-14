9f7642924bb06bc27c320670aca7af5fbdcb124a
RyzenMu <106828992+RyzenMu@users.noreply.github.com>
2024-01-14 17:29:51 +0530

    Linked List

--- CollectionsFramework/CollectionFramework.java
+++ CollectionsFramework/CollectionFramework.java
@@ -12,4 +12,5 @@ public class CollectionFramework {
     //It represents a set of classes and interfaces.
     // it is optional.
 
+
 }
---
+++ CollectionsFramework/LinkedList1.java
@@ -0,0 +1,6 @@
+public class LinkedList1 {
+    public static void main(String[] args) {
+        // linked list stores data by reference ,
+        // so it does not need to change entire list for one simple insertion.
+    }
+}
