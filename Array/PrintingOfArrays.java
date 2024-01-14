bb24c8df5c09c7164a0b78fcdd35eb5512904431
RyzenMu <106828992+RyzenMu@users.noreply.github.com>
2024-01-14 05:41:23 +0530

    printing arrays using for each

---
+++ Array/PrintingOfArrays.java
@@ -0,0 +1,16 @@
+public class PrintingOfArrays {
+    public static void main(String[] args) {
+
+        // printing using for each
+        Double [] da = {4.6, 2.8, 6.3, 9.8};
+        for(double d : da){
+            System.out.println(d);
+        }
+        System.out.println("*********************");
+        //printing using for each 2
+        String [] s = {"Hey,", "Hello", "Come Again", "go"};
+        for(String q : s){
+            System.out.println(q);
+        }
+    }
+}
