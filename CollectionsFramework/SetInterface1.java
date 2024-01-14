35332a83efd4ad0100d39f26a622c700ae55ee44
RyzenMu <106828992+RyzenMu@users.noreply.github.com>
2024-01-14 18:05:08 +0530

    set interface 1

---
+++ CollectionsFramework/SetInterface1.java
@@ -0,0 +1,14 @@
+public class SetInterface1 {
+    /*
+    * HashSet, TreeSet, LinkedHashSet
+    * A set is a collection that cannot contain duplicate elements. It models the mathematical set abstraction
+    * Duplicate values cannot be stored !!!!!!!!
+    * add() - adds an object to the collection
+    * clear() - removes all the objects from the collection
+    * contains() -  returns true if a specified object is an element within the collection
+    *isEmpty() - returns true if the collection has no elements
+    * iterator() - returns an iterator object for the collection, which may be used to retrieve an object.
+    * remove() - removes an specified object from the collection.
+    * size() - returns the number of elements in the collection.
+    * */
+}
