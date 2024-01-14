5407e2117c4e2c99c587d9f0ff2a1f6c9b42faeb
RyzenMu <106828992+RyzenMu@users.noreply.github.com>
2024-01-14 13:47:32 +0530

    collections framework

---
+++ CollectionsFramework/CollectionFramework.java
@@ -0,0 +1,15 @@
+public class CollectionFramework {
+    // Framework is a library you can download and use it. Frameworks are made by other programmers.
+    //inside them there are methods, classes and interfaces.
+    // The collection in java is a framework that provides an architecture to store and manipulate the group of objects.
+    // java collections can achieve all the operations that you perform on a data such as
+    // searching, sorting, insertion, manipulation, and deletion.
+    // java collections means a single unit of objects. Java collection framework provides many
+    // interfaces(set, list, queue, dequeue)
+    // and classes (ArrayList, vector, LinkedList, priority Queue, Hashset, LinkedHashSet,
+    // TreeSet)
+    // It provides ready-made Architecture.
+    //It represents a set of classes and interfaces.
+    // it is optional.
+
+}
