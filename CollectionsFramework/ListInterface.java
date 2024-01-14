8c718e3735fa57574c3f03c094a7c827f4cfef19
RyzenMu <106828992+RyzenMu@users.noreply.github.com>
2024-01-14 14:23:38 +0530

    list interface

---
+++ CollectionsFramework/ListInterface.java
@@ -0,0 +1,16 @@
+public class ListInterface {
+    /*
+    * List Interface is the child interface of collection interface.
+    * it inhibits a list type data structure in which we can store the ordered collection of objects.
+    * it can have duplicate values.
+    * list interface is implemented by the classes ArrayList, LinkedList, vector and stack.
+    * It is an ordered collection of objects in which duplicate values can be stored.
+    * Since list preserves the insertion order, it allows positional access ND INSERTION OF ELEMENTS.
+    *
+    *List <data-type> list1 = new ArrayList();
+    *List <data-type> list2 = new LinkedList();
+    *List <data-type> list3 = new Vector();
+    *List <data-type> list4 = new Stack();
+    *
+    * */
+}
