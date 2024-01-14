c1af0c5607a6f5dd4fcdfe2004362c1ba91efd6c
RyzenMu <106828992+RyzenMu@users.noreply.github.com>
2024-01-14 09:38:45 +0530

    final keyword

---
+++ PackagesAndNamingConventions/FinalKeyword.java
@@ -0,0 +1,11 @@
+public class FinalKeyword {
+    final int age = 19;
+    public class Car {
+        String carname;
+        final int doors;
+        Car(int doors){
+            this.doors = doors;
+        }
+
+    }
+}
