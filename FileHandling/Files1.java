9e8a4bea2d87cd736dbab9d8cfdea9cf35bdba2d
RyzenMu <106828992+RyzenMu@users.noreply.github.com>
2024-01-15 14:48:26 +0530

    basic stream

---
+++ FileHandling/Files1.java
@@ -0,0 +1,10 @@
+public class Files1 {
+    /*
+    * System.out.println() -- output stream
+    * System.in -- input Stream
+    * System.err -- error Stream
+    *
+    * java uses the concept of stream for performance.
+    * java I/o file handing is done using i/o API.
+    * */
+}
