a3f6b407e5b1f0fd8fe7730f5a3582815713a4ec
RyzenMu <106828992+RyzenMu@users.noreply.github.com>
2024-01-15 09:55:12 +0530

    Difference betweeen error and exception

---
+++ ExceptionHandling/ErrorAndException.java
@@ -0,0 +1,8 @@
+public class ErrorAndException {
+    /*
+    * Error : An Error indicates serious problem that a reasonable appliocation should not try to catch.
+    * Exception : it indicates conditions that a reasonble application might try to catch.
+    * that disrupts the normal flow of the program 
+    *
+    * */
+}
