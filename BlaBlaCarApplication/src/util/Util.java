1f24b07e39e53860b1560b9d7b7e154a5e5d250d
RyzenMu <106828992+RyzenMu@users.noreply.github.com>
2024-02-19 19:59:08 +0530

    simplified methods

--- BlaBlaCarApplication/src/util/Util.java
+++ BlaBlaCarApplication/src/util/Util.java
@@ -10,4 +10,12 @@ public class Util {
         String answer = sc.nextLine();
         return answer;
     }
+
+    public static String fillInfo(){
+        String nameSurName = enterString("Enter your name and surname");
+        String email = enterString("Enter your email");
+        String phoneNumber = enterString("Enter your phone number");
+        String location = enterString("Enter your location");
+        return "name : " + nameSurName + "\n" + " Email : " + email + "\n"+  "Phone : " + phoneNumber ;
+    }
 }
