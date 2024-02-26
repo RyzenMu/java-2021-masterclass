56ae1f0f3d234f57d56dda3ebd8a8c0864bdb4ad
RyzenMu <106828992+RyzenMu@users.noreply.github.com>
2024-02-26 07:33:28 +0530

    bla-bla-car

--- BlaBlaCarApplication/src/util/Util.java
+++ BlaBlaCarApplication/src/util/Util.java
@@ -11,11 +11,27 @@ public class Util {
         return answer;
     }
 
-    public static String fillInfo(){
+    public static int enterInteger(String title){
+        System.out.println(title);
+        Scanner sc = new Scanner(System.in);
+        int answer = sc.nextInt();
+        return answer;
+    }
+
+    public static String fillUserInfo(){
         String nameSurName = enterString("Enter your name and surname");
         String email = enterString("Enter your email");
         String phoneNumber = enterString("Enter your phone number");
         String location = enterString("Enter your location");
         return "name : " + nameSurName + "\n" + " Email : " + email + "\n"+  "Phone : " + phoneNumber ;
+    }
+
+    public static String fillTripInfo(){
+        String location = Util.enterString("Your current location");
+        int passenger = Util.enterInteger("How many passengers will travel");
+        String requires = Util.enterString("Enter requires for example SMOKING: NO"
+                +" PET:YES, LUGGAGE: NO");
+        return "Your destination from "+ location + " with " +passenger+" passenger "+
+                "with these requirements" + requires;
     }
 }
