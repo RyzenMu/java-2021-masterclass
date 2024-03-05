b0e42a02483147f756318f7b205312508052ddbf
RyzenMu <106828992+RyzenMu@users.noreply.github.com>
2024-03-05 08:20:21 +0530

    util

--- SchoolManagement/src/util/InputUtil.java
+++ SchoolManagement/src/util/InputUtil.java
@@ -1,5 +1,7 @@
 package util;
 
+import main.Student;
+
 public class InputUtil {
     public static int enternteger(String title){
         System.out.println(title);
@@ -8,8 +10,24 @@ public class InputUtil {
         return operation;
     }
 
+    public static String enterString(String title) {
+        System.out.println(title);
+        Scanner scanner = new Scanner(System.in);
+        int answer = scanner.nextLine();
+    }
+
     public static void main(String[] args) {
         int age = enternteger("How old are you");
         System.out.println(age);
+    }
+
+    public static student fill(){
+        String name = InputUtil.enterString("Enter Student name");
+        String surName = InputUtil.enterString("Enter Student surname");
+        String email = InputUtil.enterString("Enter Student Email");
+        String classname = InputUtil.enterString("enter Student class name");
+        int age = InputUtil.enternteger("Enter Student age");
+        Student student = new Student(name, surName,age, classname);
+        return student
     }
 }
--- SchoolManagement/src/util/MenuUtil.java
+++ SchoolManagement/src/util/MenuUtil.java
@@ -1,4 +1,27 @@
 package util;
 
+import main.Configuration;
+import main.Student;
+
 public class MenuUtil {
+    public static void printAllStudents(){
+        if (Configuration.students == null){
+            System.out.println("You do not have any student");
+            return;
+        }
+        System.out.println("students that you have registered");
+        for (int i = 0; i < Configuration.students.length; i++) {
+            Student st = Configuration.students[i];
+            System.out.println((1 + i) + "Student \n" + st.getInfo());
+        }
+    }
+    public static void registerStudent(){
+        int count = InputUtil.enterString("How many student will you register");
+        Configuration.students = new Student[count];
+        for (int i = 0; i < count; i++) {
+            System.out.println((i + 1) + "registration");
+            Student st = InputUtil.fill();
+            Configuration.students[i] = st;
+        }
+    }
 }
