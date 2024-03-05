ba43bb1544148efaa645de02acc336920bac4ada
RyzenMu <106828992+RyzenMu@users.noreply.github.com>
2024-03-05 06:54:05 +0530

    main

--- SchoolManagement/src/main/Main.java
+++ SchoolManagement/src/main/Main.java
@@ -1,7 +1,21 @@
 package main;
 
+import util.InputUtil;
+
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Welcome to School Operation center");
+        int operation = InputUtil.enternteger("what do you want to do\n" +
+                "\n1.Register Student" +
+                "\n2.Show all Student" +
+                "\n3.Find Student" +
+                "\n4.Update Student" +
+                "\n0.Quit");
+//        Scanner scanner = new Scanner(System.in);
+//        int operation = scanner.nextInt();
+        if (operation == 1) {
+            System.out.println("You choose operation 1 and you can register " +
+                    "student");
+        }
     }
 }
