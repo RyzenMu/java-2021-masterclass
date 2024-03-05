83ec34d9b69e2796c94c9920a2b372bd8894da50
RyzenMu <106828992+RyzenMu@users.noreply.github.com>
2024-03-05 06:54:48 +0530

    util

--- SchoolManagement/src/util/InputUtil.java
+++ SchoolManagement/src/util/InputUtil.java
@@ -1,4 +1,15 @@
 package util;
 
 public class InputUtil {
+    public static int enternteger(String title){
+        System.out.println(title);
+        Scanner scanner = new Scanner(System.in);
+        int operation = scanner.nextInt();
+        return operation;
+    }
+
+    public static void main(String[] args) {
+        int age = enternteger("How old are you");
+        System.out.println(age);
+    }
 }
